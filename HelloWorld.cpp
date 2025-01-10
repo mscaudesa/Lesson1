01c05799dd41c973a7508a029432401d5ac7637d
mscaudesa <mscaramal@udesa.edu>
2025-01-10 15:19:03 -0300

    First commit

---
+++ HelloWorld.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
+
+void myFunc(string myMsg){
+    cout << myMsg << endl;
+}
+
+int main(){
+    vector<string> msg = {"Hello", "C++"};
+    int x = 4;
+
+    myFunc("Hello, world!");
+    
+    return 0;
+}
